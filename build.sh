7a47edac02053ad95bd7a3ad22845d4068d0597d
kschat <kyleschattler@gmail.com>
2013-09-10 18:50:50 -0400

    Added a "standable" executable

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-zip app.nw -r index.html package.json LICENSE README.md css/ fonts/ js/
+FILENAME=scrum-poker
+
+#Create the standalone .nw file
+zip $FILENAME.nw -r index.html package.json LICENSE README.md css/ fonts/ js/
+
+#create the packaged executable
+cat /usr/local/bin/nw $FILENAME.nw > $FILENAME"-packaged" && chmod +x $FILENAME"-packaged"
+cp /usr/local/bin/nw.pak .
+zip $FILENAME"-packaged.zip" $FILENAME"-packaged" nw.pak
+
+#clean up files
+rm $FILENAME"-packaged" nw.pak
