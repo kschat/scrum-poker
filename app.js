f4e7f4bc6ff5d9a5d97e5f2100d5a77d95fad245
kschat <kyleschattler@gmail.com>
2014-01-29 21:43:04 -0500

    updated package.json

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-var http = require('http'),
-	fs = require('fs'),
+var	fs = require('fs'),
 	connect = require('connect'),
-	connectRoute = require('connect-route')
+	connectRoute = require('connect-route');
 
 var app = connect()
 	.use(connect.logger('dev'))
@@ -11,10 +10,6 @@ var app = connect()
 
 app.use(connectRoute(function(router) {
 	router.get('/', function(req, res, next) {
-		res.end(fs.readFileSync(__dirname + '/views/home.html'));
-	});
-
-	router.get('/home', function(req, res, next) {
 		res.end(fs.readFileSync(__dirname + '/views/home.html'));
 	});
 }));
