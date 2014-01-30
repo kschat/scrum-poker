240a9dac02fef73a6b769d3e8df08a179893e8b0
kschat <kyleschattler@gmail.com>
2014-01-29 20:31:39 -0500

    Added gzip and caching

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var http = require('http'),
 	fs = require('fs'),
 	connect = require('connect'),
-	connectRoute = require('connect-route');
+	connectRoute = require('connect-route'),
+	gzippo = require('gzippo');
 
 var app = connect()
 	.use(connect.logger('dev'))
-	.use(connect.static(__dirname + '/public'));
+	.use(gzippo.staticGzip(__dirname + '/public'))
+	.use(gzippo.staticGzip(__dirname + '/views'));
 
 app.use(connectRoute(function(router) {
 	
